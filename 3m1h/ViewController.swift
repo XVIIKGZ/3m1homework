efc4715bb42cae067d39da86334a49e7993e1eea
XVIIKGZ <nazar.kydyraliev@gmail.com>
2022-12-23 21:36:48 +0600

    Initial Commit

---
+++ 3m1h/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3m1h
+//
+//  Created by Nazar Kydyraliev on 23/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 3m1hUITests/_m1hUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _m1hUITestsLaunchTests.swift
+//  3m1hUITests
+//
+//  Created by Nazar Kydyraliev on 23/12/22.
+//
+
+import XCTest
+
+final class _m1hUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
